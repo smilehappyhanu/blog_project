d8cc1c7f2a1cf5c8cf01b06c86242e6396a80479
TienHT <hoangtienhanu881996@gmail.com>
2025-04-23 16:48:04 +0700

    Integrate admin template

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,15 +1,49 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Admin Dashboard') }}
-        </h2>
-    </x-slot>
-
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <x-welcome />
-            </div>
-        </div>
+<!DOCTYPE html>
+<html>
+  <head> 
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Dark Bootstrap Admin </title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="robots" content="all,follow">
+    <!-- Bootstrap CSS-->
+    <link rel="stylesheet" href="{{asset('admin-template/vendor/bootstrap/css/bootstrap.min.css')}}">
+    <!-- Font Awesome CSS-->
+    <link rel="stylesheet" href="{{asset('admin-template/vendor/font-awesome/css/font-awesome.min.css')}}">
+    <!-- Custom Font Icons CSS-->
+    <link rel="stylesheet" href="{{asset('admin-template/css/font.css')}}">
+    <!-- Google fonts - Muli-->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Muli:300,400,700">
+    <!-- theme stylesheet-->
+    <link rel="stylesheet" href="{{asset('admin-template/css/style.default.css')}}" id="theme-stylesheet">
+    <!-- Custom stylesheet - for your changes-->
+    <link rel="stylesheet" href="{{asset('admin-template/css/custom.css')}}">
+    <!-- Favicon-->
+    <link rel="shortcut icon" href="{{asset('admin-template/img/favicon.icon')}}">
+    <!-- Tweaks for older IEs--><!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script><![endif]-->
+  </head>
+  <body>
+    @include('admin.header')
+    <div class="d-flex align-items-stretch">
+      <!-- Sidebar Navigation-->
+    @include('admin.sidebar')
+      <!-- Sidebar Navigation end-->
+      <div class="page-content">
+        @include('admin.body')
+        @include('admin.footer')
+      </div>
     </div>
-</x-app-layout>
+    <!-- JavaScript files-->
+    <script src="{{asset('admin-template/vendor/jquery/jquery.min.js')}}"></script>
+    <script src="{{asset('admin-template/vendor/popper.js/umd/popper.min.js')}}"> </script>
+    <script src="{{asset('admin-template/vendor/bootstrap/js/bootstrap.min.js')}}"></script>
+    <script src="{{asset('admin-template/vendor/jquery.cookie/jquery.cookie.js')}}"> </script>
+    <script src="{{asset('admin-template/vendor/chart.js/Chart.min.js')}}"></script>
+    <script src="{{asset('admin-template/vendor/jquery-validation/jquery.validate.min.js')}}"></script>
+    <script src="{{asset('admin-template/js/charts-home.js')}}"></script>
+    <script src="{{asset('admin-template/js/front.js')}}"></script>
+  </body>
+</html>
